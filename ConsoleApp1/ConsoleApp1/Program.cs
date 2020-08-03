04d935024471d4a34b542c39d7f342250de04b80
Milan992 <mitic.gsm@gmail.com>
2020-08-03 12:06:49 +0200

    Created method IsJmbg for checking if entered string is in JMBG format

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static CountdownEvent countdown = new CountdownEvent(6);
+        static void Main(string[] args)
+        {
+            new Thread(SaySomething).Start("i am one");
+            new Thread(SaySomething).Start("i am dva");
+            new Thread(SaySomething).Start("i am tri");
+            new Thread(SaySomething).Start("i am cet");
+            new Thread(SaySomething).Start("i am Pet");
+            new Thread(SaySomething).Start("i am sest");
+
+            countdown.Wait();
+            Console.WriteLine("gotovo");
+            Console.ReadKey();
+        }
+
+        private static void SaySomething(object thing)
+        {
+            Console.WriteLine("thing");
+           countdown.Signal();
+        }
+    }
+}
--- DAN_XLVIII_Milan_Mitic/WpfPizzeria/Service.cs
+++ DAN_XLVIII_Milan_Mitic/WpfPizzeria/Service.cs
@@ -7,6 +7,33 @@ using System.Threading.Tasks;
 namespace WpfPizzeria
 {
     class Service
-    {
+    { /// <summary>
+      /// Checks if string is in JMBG format.
+      /// </summary>
+      /// <param name="userName"></param>
+      /// <returns></returns>
+        public bool IsJmbg(string userName)
+        {
+            bool jmbg = false;
+            if (userName.Length == 13)
+            {
+                try
+                {
+                    long i = Convert.ToInt64(userName);
+                    string date = "1" + userName.Substring(4, 3) + "-" + userName.Substring(2, 2) + "-" + userName.Substring(0, 2);
+                    DateTime dateOfBirth = DateTime.ParseExact(date, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                    jmbg = true;
+                }
+                catch
+                {
+                    jmbg = false;
+                }
+            }
+            else
+            {
+                jmbg = false;
+            }
+            return jmbg;
+        }
     }
 }
