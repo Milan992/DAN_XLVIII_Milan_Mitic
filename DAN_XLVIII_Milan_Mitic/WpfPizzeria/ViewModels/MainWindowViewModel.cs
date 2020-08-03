f4543c36b9d786591c198ffa1b6efa9023986461
Milan992 <mitic.gsm@gmail.com>
2020-08-03 09:25:24 +0200

    Created MainWindowViewModel class

---
+++ DAN_XLVIII_Milan_Mitic/WpfPizzeria/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfPizzeria.Model;
+using WpfPizzeria.Views;
+
+namespace WpfPizzeria.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+
+        Service service = new Service();
+        MainWindow main;
+
+        #region Constructors
+
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private string userName;
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand logIn;
+
+        public ICommand LogIn
+        {
+            get
+            {
+                if (logIn == null)
+                {
+                    logIn = new RelayCommand(param => LogInExecute(), param => CanLogInExecute());
+                }
+
+                return logIn;
+            }
+        }
+
+        private void LogInExecute()
+        {
+            try
+            {
+                if (service.IsJmbg(UserName) && Password == "Gost")
+                {
+                    Guest guest = new Guest();
+                    guest.ShowDialog();
+                    File.WriteAllText(@"..\..\Username.txt", UserName);
+                }
+                else if (UserName == "Zaposleni" && Password == "Zaposleni")
+                {
+                    Employee employee = new Employee();
+                    employee.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password incorrect.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanLogInExecute()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
